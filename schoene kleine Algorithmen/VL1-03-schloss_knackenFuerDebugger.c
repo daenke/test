a962daac63580bd6be941e4b144103cb7b201a18
Dani <56628625+oldy-22@users.noreply.github.com>
2022-06-13 08:40:43 +0200

    Add files via upload

--- schoene kleine Algorithmen/VL1-03-schloss_knackenFuerDebugger.c
+++ schoene kleine Algorithmen/VL1-03-schloss_knackenFuerDebugger.c
@@ -20,13 +20,14 @@
 
 bool DEBUG = true;
 unsigned int* abb;
-unsigned int bitNumber = 0;
+unsigned int bit2Test = 0;
 
 unsigned int switchBit(int key, int whichBit);
 int test(unsigned int, bool silent);
 void generate(void);
 char * bin2String(unsigned int);
 char * markPosition(int position);
+char * key2String(unsigned int, int);
 
 int main (void) // zum Laufen in main umbenennen
 {
@@ -43,8 +44,7 @@ int main (void) // zum Laufen in main umbenennen
 
 	int i = 0;
 	for(; i < 32 ; i++){/* für jedes Bit wird geprüft ob es richtig ist oder ob es getaucht werden muss*/
-
-		bitNumber=i+1;
+		bit2Test=i;
 		int rightBits  = test(key, false);/* Anzahl der richtigen Bits bevor das i-te Bit getauscht wurde*/
 		int newRightBits = test(switchBit(key,i), true);/* Anzahl der richtigen Bits nachdem das i-te Bit getauscht wurde*/
 
@@ -90,8 +90,8 @@ int test(unsigned int toTest,  bool silent)
 		}
         //ab hier nur für Debug
          if (DEBUG && !silent) {
-            printf("Bit-Nummer = %d Key = %u, richtige Bits = %d\n", bitNumber, toTest, rightBits);
-            printf("            key: %42s  0x%x\n", bin2String(toTest), toTest);
+            printf("Bit-Position = %d Key = %u, richtige Bits = %d\n", bit2Test, toTest, rightBits);
+            printf("            key: %42s  0x%x\n", key2String(toTest, bit2Test), toTest);
             printf("           goal: %42s  0x%x\n", bin2String(*abb), *abb);
             unsigned int a=toTest & x; printf("   masked key a: %42s  0x%x\n", bin2String(a), a);
             unsigned int b=*abb| ~x; printf("  masked goal b: %42s  0x%x\n", bin2String(b), b);
@@ -113,7 +113,7 @@ int test(unsigned int toTest,  bool silent)
         exit(0); // unschön aber wird gebraucht, dass Console stehen bleibt
     }else{
         if (!silent) {
-            printf ("Bit %2d getestet, %d richtige Bits \n", bitNumber, rightBits);
+            printf ("Bit %2d getestet, %d richtige Bits \n", bit2Test, rightBits);
             int c; if (DEBUG) c = getchar();
         }
    }
@@ -168,3 +168,23 @@ char * markPosition(int position)
 	} while(i < 43);
 	return retbuf;
 }
+
+char * key2String(unsigned int num, int pos2Test)
+{
+	static char retbuf[43];
+	int i =0, bit=0;
+	for(; i<43 ; i++) retbuf[i]='0'; //init mit 0
+	char *p; i=0;
+	p = &retbuf[sizeof(retbuf)-1]; // ans ENDE VOM String zeigen
+	*p = '\0'; // Stringabschluss
+	do {
+		*--p = "0123456789abcdef"[num % 2]; // nur die ersten 2 Zeichen werden benötigt..
+		if (bit == pos2Test) *p = '?';
+        bit++;
+		num /= 2;
+        if ((++i)%4==0) *--p=' ';
+        if (i%8==0) *--p=' ';
+	} while(i < 43);
+	return retbuf;
+}
+
